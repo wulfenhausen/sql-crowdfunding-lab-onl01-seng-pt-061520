e59c7134e45ecd5b73006003efc1a6557b6a8057
William Han Findiesen <william.findiesen@gmail.com>
2020-09-29 05:18:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
     projects.title, SUM(pledges.amount) 
   FROM projects
   INNER JOIN pledges
-    ON projects.id = pledges.projects_id
+    ON projects.id = pledges.project_id
     GROUP BY projects.title;  "
 end
 
