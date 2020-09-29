d18a94ace1110d6eb56fb26f1ef94f24302f8a32
William Han Findiesen <william.findiesen@gmail.com>
2020-09-29 05:38:56 +0000

    Done.

--- lib/data.sql
+++ lib/data.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
 (5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
 (11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
