fdcc77361aaf7b9f9b22825a0452dcb6e4100f59
William Han Findiesen <william.findiesen@gmail.com>
2020-09-29 05:14:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT 
-    projects.titles, SUM(pledge_amount) FROM 
+    pledges.project_id, SUM(pledge_amount) FROM 
   "
 end
 
